5e41d15fc0a6a0bd5b32b111ff770513348e4e8c
USER <fjqngodys2@gmail.com>
2023-06-06 16:35:49 +0900

    엔티티 직접 사용 소스 코드 추가

---
+++ ch11-model2/src/main/java/jpql/JpaMain3.java
@@ -0,0 +1,70 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+
+public class JpaMain3 {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3= new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+            //String query = "select m from Member m where m = :member";
+            String query = "select m from Member m where m.id = :memberId";
+
+            List<Member> findMember = em.createQuery(query, Member.class)
+                    .setParameter("memberId", member1.getId())
+                    .getResultList();
+
+            System.out.println("findMember = " + findMember);
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace(); //이 부분을 추가하면 예외를 확인할 수 있습니다.
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
